7717385117c327ac6a26bb3cd90db3d501d03b74
Empty40 <Sorok041@gmail.com>
2023-06-24 02:05:00 +0300

    Групповое ТЗ, добавление функциональности "Отзывы" тест

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Review.java
@@ -0,0 +1,29 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.*;
+
+@Data
+public class Review {
+
+    private int reviewId;
+
+    private String content;
+
+    private Boolean isPositive;
+
+    private int userId;
+
+    private int filmId;
+
+    private int useful = 0;
+
+    public Review(int reviewId, @NonNull String content, @NonNull Boolean isPositive, @NonNull Integer userId,
+                  @NonNull Integer filmId, int useful) {
+        this.reviewId = reviewId;
+        this.content = content;
+        this.isPositive = isPositive;
+        this.userId = userId;
+        this.filmId = filmId;
+        this.useful = useful;
+    }
+}
