83f4e0f963a60af9120be60772527497cdaadaa5
Empty40 <Sorok041@gmail.com>
2023-06-28 07:15:20 +0300

    Правки замечаний ревьюера

--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate.service;
 
-import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.dao.EventDao;
