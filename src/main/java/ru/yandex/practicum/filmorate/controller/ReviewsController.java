c7d97744807d74cff7060804c0f4f650520f3288
Empty40 <Sorok041@gmail.com>
2023-06-30 22:38:58 +0300

    Правки замечаний ревьюера vol.2

--- src/main/java/ru/yandex/practicum/filmorate/controller/ReviewsController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ReviewsController.java
@@ -23,47 +23,56 @@ public class ReviewsController {
 
     @PostMapping
     public Review addReview(@RequestBody @Valid Review review) {
+        log.info("Добавление отзыва");
         return reviewService.addReview(review);
     }
 
     @PutMapping
     public Review updateReview(@RequestBody Review review) {
+        log.info("Обновление отзыва");
         return reviewService.updateReview(review);
     }
 
     @DeleteMapping("/{id}")
     public void deleteReview(@PathVariable int id) {
+        log.info("Удаление отзыва");
         reviewService.deleteReview(id);
     }
 
     @GetMapping("/{id}")
     public Review getReview(@PathVariable int id) {
+        log.info("Получение отзыва по айди");
         return reviewService.getReview(id);
     }
 
     @GetMapping()
     public List<Review> getFilmsReviews(@RequestParam(defaultValue = "0") Integer filmId,
                                         @RequestParam(defaultValue = "10") int count) {
+        log.info("Получение отзывов");
         return reviewService.getFilmsReviews(filmId, count);
     }
 
     @PutMapping("/{id}/like/{userId}")
     public void addLikeToReview(@PathVariable int id, @PathVariable int userId) {
+        log.info("Добавление лайка к отзыву");
         reviewService.addLikeToReview(id, userId);
     }
 
     @PutMapping("/{id}/dislike/{userId}")
     public void addDislikeToReview(@PathVariable int id, @PathVariable int userId) {
+        log.info("Добавление дизлайка к отзыву");
         reviewService.addDislikeToReview(id, userId);
     }
 
     @DeleteMapping("/{id}/like/{userId}")
     public void deleteLikeToReview(@PathVariable int id, @PathVariable int userId) {
+        log.info("Удаление лайка с отзыва");
         reviewService.deleteLikeToReview(id, userId);
     }
 
     @DeleteMapping("/{id}/dislike/{userId}")
     public void deleteDislikeToReview(@PathVariable int id, @PathVariable int userId) {
+        log.info("Удаление дизлайка с отзыва");
         reviewService.deleteDislikeToReview(id, userId);
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -29,41 +29,49 @@ public class UserController {
 
     @GetMapping("/{id}")
     public User getUser(@PathVariable int id) {
+        log.info("Получение пользователя");
         return userService.getUser(id);
     }
 
     @GetMapping
     public List<User> getUsers() {
+        log.info("Получение всех пользователей");
         return userService.getUsers();
     }
 
     @PostMapping
     public User createUser(@RequestBody User user) {
+        log.info("Создание пользователя");
         return userService.createUser(user);
     }
 
     @PutMapping
     public User updateUser(@RequestBody User user) {
+        log.info("Обновление пользователя");
         return userService.updateUser(user);
     }
 
     @GetMapping("/{id}/friends/common/{otherId}")
     public List<User> getCommonFriends(@PathVariable int id, @PathVariable int otherId) {
+        log.info("Получение общих друзей");
         return userService.getCommonFriends(id, otherId);
     }
 
     @PutMapping("/{id}/friends/{friendId}")
     public void addFriend(@PathVariable int id, @PathVariable int friendId) {
+        log.info("Добавление друга");
         userService.addFriend(id, friendId);
     }
 
     @GetMapping("/{id}/friends")
     public List<User> getFriends(@PathVariable int id) {
+        log.info("Получение списка друзей");
         return userService.getFriends(id);
     }
 
     @DeleteMapping("/{id}/friends/{friendId}")
     public void deleteFriend(@PathVariable int id, @PathVariable int friendId) {
+        log.info("Удаление друга");
         userService.deleteFriend(id, friendId);
     }
 
@@ -76,11 +84,13 @@ public class UserController {
     //Получение ленты событий
     @GetMapping("/{userId}/feed")
     public Collection<Event> getUserFeed(@PathVariable int userId) {
+        log.info("Получение списка событий пользователя");
         return userService.getUserFeed(userId);
     }
 
     @GetMapping("/{id}/recommendations")
     public List<Film> showFilmRecommendations(@PathVariable int id) {
+        log.info("Получение списка рекомендаций");
         return filmService.showFilmRecommendations(id);
     }
 }
